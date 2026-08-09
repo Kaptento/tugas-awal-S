da1c7072b8c6334baec5a95bb93fa53aa3c11a64
Kaptento <louisgracia456@gmail.com>
2026-08-10 06:43:34 +0700

    commit

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -8,7 +8,32 @@ class TeacherController extends Controller
 {
  public function index()
     {
-        return "Ini adalah halaman daftar guru";
+        $title = 'Sistem Sekolah - Daftar Guru';
+        $teachers = [
+    [
+      'id' => 1,
+      'nip' => '198501012024',
+      'name' => 'Budi Santoso',
+      'gender' => 'Laki-Laki',
+      'subject' => 'Akuntansi Dasar',
+      'phone' => '081234560001',
+      'status' => 'Aktif',
+    ],
+    [
+      'id' => 2,
+      'nip' => '198703152024',
+      'name' => 'Siti Aminah',
+      'gender' => 'Perempuan',
+      'subject' => 'Jaringan Komputer',
+      'phone' => '081234560002',
+      'status' => 'Aktif',
+    ]
+];
+ return view('teachers.index', [
+            'title' => $title,
+            'teachers' => $teachers,
+        ]);
+
     }
 
     public function show(string $id)
