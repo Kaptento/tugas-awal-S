1634c8e8b03d8dc653ed6a24487d16047d2783ec
Kaptento <louisgracia456@gmail.com>
2026-08-14 20:07:31 +0700

    implement major

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -6,39 +6,94 @@ use Illuminate\Http\Request;
 
 class MajorController extends Controller
 {
-     public function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return "Ini adalah halaman daftar jurusan";
+        $title = "Sistem Sekolah - Daftar Jurusan";
+        $majors = [
+            [
+                'id' => 1,
+                'code' => 'AKL',
+                'name' => 'Akuntansi dan Keuangan Lembaga',
+                'description' => 'Program keahlian yang membekali murid dengan kompetensi pencatatan dan pelaporan keuangan.',
+            ],
+            [
+                'id' => 2,
+                'code' => 'TKJ',
+                'name' => 'Teknik Komputer dan Jaringan',
+                'description' => 'Program keahlian yang membekali murid dengan kompetensi instalasi, konfigurasi, dan pemeliharaan jaringan komputer.',
+            ],
+            [
+                'id' => 3,
+                'code' => 'BD',
+                'name' => 'Bisnis Digital',
+                'description' => 'Program keahlian yang membekali murid dengan kompetensi pemasaran dan pengelolaan bisnis berbasis digital.',
+            ],
+        ];
+
+        return view('majors.index', [
+            'title' => $title,
+            'majors' => $majors
+        ]);
+
     }
 
-    public function show(string $id)
-    {
-        return "Menampilkan detail jurusan dengan ID : {$id}";
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
-        return "Menampilkan halaman untuk tambah data jurusan";
+        $title = "Sistem Sekolah - Tambah Jurusan";
+        return view('majors.create', [
+            'title' => $title,
+        ]);
     }
 
-    public function edit(string $id)
-    {
-        return "Menampilkan halaman edit jurusan";
-    }
-
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
-        return "Melakukan penambahan data jurusan baru";
+        return "Melakukan penambahan data jurusan";
     }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $title = "Sistem Sekolah - Detail Jurusan";
+        return view('majors.show', [
+            'title' => $title,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $title = "Sistem Sekolah - Ubah Jurusan";
+        return view('majors.edit', [
+            'title' => $title,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(Request $request, string $id)
     {
         return "Melakukan perubahan data jurusan";
     }
 
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(string $id)
     {
         return "Menghapus data jurusan";
     }
 }
-
